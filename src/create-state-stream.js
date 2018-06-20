65cbc8c228dd0a81a7b87e77af58fcd97abb6dfb
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-06-20 10:42:23 +0200

    Extract createState$ function

---
+++ src/create-state-stream.js
@@ -0,0 +1,16 @@
+const {create} = require('@most/create');
+
+const update = require('./update');
+
+let state = {
+  players: [],
+};
+
+module.exports = function createState$(action$) {
+  return create((add, end, error) => {
+    action$.observe((action) => {
+      state = update(state, action);
+      add(state);
+    });
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const http = require('http');
-const {create} = require('@most/create');
 const path = require('path');
 const socketio = require('socket.io');
 
 const createSocketAction$ = require('./create-socket-action-stream');
-const update = require('./update');
+const createState$ = require('./create-state-stream');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,22 +13,9 @@ const io = socketio(server);
 const publicPath = process.env.PUBLIC || 'public';
 app.use(express.static(path.join(__dirname, publicPath)));
 
-let state = {
-  players: [],
-};
-
 function personalizeState(state) {
   const players = state.players.map((socket) => socket.id);
   return {...state, players};
-}
-
-function createState$(action$) {
-  return create((add, end, error) => {
-    action$.observe((action) => {
-      state = update(state, action);
-      add(state);
-    });
-  });
 }
 
 const action$ = createSocketAction$(io);
