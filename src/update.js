8033b2ce013f97751e7512e84bd8baef6011e775
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-06-20 11:17:00 +0200

    Only allow 4 players

--- src/update.js
+++ src/update.js
@@ -1,6 +1,9 @@
 module.exports = function update(state, action) {
   switch (action.type) {
   case 'player connected': {
+    if (state.players.length >= 4) {
+      return state;
+    }
     const players = state.players.concat([action.payload]);
     return {...state, players};
   }
