652d52c7e900bc1135eddbe941edebb1f67504ab
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-06-29 11:44:11 +0200

    Send object in player array

--- src/personalize-state.js
+++ src/personalize-state.js
@@ -2,7 +2,9 @@ const removeKeys = require('./func/remove-keys');
 
 module.exports = function personalizeState(id, state) {
   const keysToHide = ['deck'];
-  const players = state.players.map((player) => player.socket.id);
+  const players = state.players.map((player) => {
+    return {...player, socket: {id: player.socket.id}};
+  });
 
   return {...removeKeys(keysToHide, state), id, players};
 };
