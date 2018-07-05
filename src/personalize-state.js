f251b8bed0e88f337eab8bab0cc6ade6856ad18b
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-07-05 09:54:51 +0200

    Hide other players cards

--- src/personalize-state.js
+++ src/personalize-state.js
@@ -3,6 +3,14 @@ const removeKeys = require('./func/remove-keys');
 module.exports = function personalizeState(id, state) {
   const keysToHide = ['deck'];
   const players = state.players.map((player) => {
+    // Hide other players cards
+    if (player.hasOwnProperty('cards')) {
+      const cards = id === player.socket.id ?
+        player.cards :
+        player.cards.map((card) => 'back');
+      return {...player, cards, socket: {id: player.socket.id}};
+    }
+
     return {...player, socket: {id: player.socket.id}};
   });
 
